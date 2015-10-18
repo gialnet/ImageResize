6ded2cccb113790a3f90a1103042be83e4c41bc8
gialnet <antonio.gialnet@gmail.com>
2015-10-18 18:53:30 +0200

    Borrar ficheros y crear directorios

--- src/main/java/es/redmoon/imageresize/App.java
+++ src/main/java/es/redmoon/imageresize/App.java
@@ -21,24 +21,44 @@ public class App
     {
         
         Path start = FileSystems.getDefault().getPath("C:\\Users\\Usuario\\Pictures\\CASAS Y PISOS");
+        
 		Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
                     int photos=1;
 			@Override
 			public FileVisitResult visitFile(Path file,
 					BasicFileAttributes attrs) throws IOException {
                             
+
+                        File f=null;
+
+                        // Crear un directorio
+                        if (attrs.isDirectory())
+                        {
+
+                          f = new File(file.getParent()+"\\800x600");
+                          f.mkdirs();
+                          f=null;
+                        }
                             
-				if (file.toString().toLowerCase().endsWith(".jpg")) {
-					//System.out.println(file);
-                                        //System.out.println(file.getFileName());
-                                        //System.out.println(file.getParent());
-                                        photos++;
-                                        Thumbnails.of(new File(file.toUri()))
-                                        .size(800, 600)
-                                        .toFile(new File(file.getParent()+"\\800x600-"+photos+".jpg"));
-				}
-                                System.out.println(photos);
-				return FileVisitResult.CONTINUE;
+                            // if (file.toString().toLowerCase().endsWith(".jpg")) {
+			if (file.toString().toLowerCase().contains("600x400")) {
+                                    
+                                //System.out.println(file);
+                                //System.out.println(file.getFileName());
+                                //System.out.println(file.getParent());
+                                photos++;
+                                f = new File(file.toUri());
+                                f.delete();
+                                /*
+                                Thumbnails.of(new File(file.toUri()))
+                                .size(800, 600)
+                                .toFile(new File(file.getParent()+"\\800x600-"+photos+".jpg"));
+                                */ 
+                        }
+                        
+                        System.out.println(photos);
+                        return FileVisitResult.CONTINUE;
+                        
 			}
 		});
                 
